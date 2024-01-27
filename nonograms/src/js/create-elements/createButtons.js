70423981356e0b92fe57734564a2ea3cdf9d264a
Alexey Melnikov <cancem343@gmail.com>
2024-01-27 17:32:29 +0300

    feat: add buttons

--- nonograms/src/js/create-elements/createButtons.js
+++ nonograms/src/js/create-elements/createButtons.js
@@ -1,15 +1,34 @@
 import { resetGround } from '../game-process/initGame';
 
-export default function createButtons(Block, time) {
-  const [buttonsBlock, resetBtn, saveBtn, continueBtn, resultBtn] = [
+export default function createButtons(block, time) {
+  const sectionButtons = block;
+
+  const [
+    buttonsBlock,
+    resetBtn,
+    saveBtn,
+    continueBtn,
+    resultBtn,
+    solutionBtn,
+    randomBtn,
+  ] = [
     document.createElement('div'),
+    document.createElement('button'),
+    document.createElement('button'),
     document.createElement('button'),
     document.createElement('button'),
     document.createElement('button'),
     document.createElement('button'),
   ];
 
-  const arrayBtn = [resetBtn, saveBtn, continueBtn, resultBtn];
+  const arrayBtn = [
+    resetBtn,
+    saveBtn,
+    continueBtn,
+    resultBtn,
+    solutionBtn,
+    randomBtn,
+  ];
 
   arrayBtn.forEach((btn) => {
     btn.classList.add('btn');
@@ -17,15 +36,20 @@ export default function createButtons(Block, time) {
   });
 
   resetBtn.addEventListener('click', () => resetGround(time));
-  Block.appendChild(buttonsBlock);
+  block.appendChild(buttonsBlock);
 
-  buttonsBlock.className = 'main__Block-buttons';
+  sectionButtons.className = 'main__section-buttons';
+  buttonsBlock.className = 'main__block-buttons';
   resetBtn.textContent = 'Reset';
   saveBtn.textContent = 'Save';
   continueBtn.textContent = 'Continue';
   resultBtn.textContent = 'Results';
+  solutionBtn.textContent = 'Solution';
+  randomBtn.textContent = 'Random Game';
 
   saveBtn.disabled = true;
   continueBtn.disabled = true;
   resultBtn.disabled = true;
+  solutionBtn.disabled = true;
+  randomBtn.disabled = true;
 }
